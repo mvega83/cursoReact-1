4edfb6a6b2979a67a91a981943e1df9c51dab3ef
mvega83 <164363139+mvega83@users.noreply.github.com>
2024-06-12 17:26:44 -0400

    inteface

--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -1,5 +1,47 @@
+import axios from "axios"
+import { useEffect } from "react"
+import {  ReqResUserListResponse} from "../interfaces"
+
+const cargarUsuarios = async()=>{
+    //const axios.get<ReqResUserListResponse>('https://reqres.in/api/users?page=2')
+    //.then( resp=> console.log(resp.data.data[0]));
+    
+    try {
+        const {data} = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users')
+        return data.data;  //usuarios
+
+    }
+    catch (error){
+        console.log(error);
+        return[];
+
+    }
+
+}
+
 export const UserPage =()=>{
+    useEffect(()=>{
+        
+    },[])
+
+
     return <>
-    <h3></h3>
+    <h3>Usuarios</h3>
+    <table>
+        <thead>
+            <tr>
+                <th>Avatar</th>
+                <th>Nombre</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Avatar</td>
+                <td>Nombre</td>
+                <td>Email</td>
+            </tr>
+        </tbody>
+    </table>
     </>
 }
